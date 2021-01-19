b4efb9370e7586a524a58be0f64d8147ef30277e
irfan khan <irfankhan457@gmail.com>
2021-01-20 02:01:59 +0530

    Release 1.0 Plan

---
+++ src/app/common/product.ts
@@ -0,0 +1,11 @@
+export class Product {
+    sku: string;
+    name: string;
+    description: string;
+    unitPrice: string;
+    imageUrl: string;
+    active: string;
+    unitInStock: string;
+    dateCreated: string;
+    lastUpdated: string;
+}
